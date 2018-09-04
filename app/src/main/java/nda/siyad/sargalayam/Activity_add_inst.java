0be6b6fdfe9fc303c3b48e1acbc1cad1b05826f9
siyad rahman <siyadrahman121@gmail.com>
2018-09-05 01:28:16 +0530

    initial commit

---
+++ app/src/main/java/nda/siyad/sargalayam/Activity_add_inst.java
@@ -0,0 +1,13 @@
+package nda.siyad.sargalayam;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity_add_inst extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_inst);
+    }
+}
